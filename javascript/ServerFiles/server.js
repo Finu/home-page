084402cc2fa244f62713b48efb8ccc5c1f7fd5de
Jarek <finn@wp.pl>
2016-06-09 22:55:52 +0200

    Tabs

--- javascript/ServerFiles/server.js
+++ javascript/ServerFiles/server.js
@@ -3,80 +3,81 @@ var logger = require('morgan');
 var route = require('./Routers/router.js');
 var path = require('path');
 
+
 var Server = function () { };
 
 Server.prototype.Start = function () {
-	this.setupVariables();
-	this.setupTerminationHandlers();
-	
-	this.app = express()
-	this.app.use(express.static(path.join(__dirname, '../..', 'public')));
-	this.app.set('view engine', 'ejs');
-	this.app.use(logger('dev'));
-	
-	/*app.use(function (req, res, next) {
-		var err = new Error('Not Found');
-		err.status = 404;
-		next(err);
-	});*/
+    this.setupVariables();
+    this.setupTerminationHandlers();
 
-	if (this.app.get('env') === 'development') {
-		this.app.use(function (err, req, res, next) {
-			res.status(err.status || 500);
-			res.render('error', {
-				message: err.message,
-				error: err
-			});
-		});
-	}
+    this.app = express()
+    this.app.use(express.static(path.join(__dirname, '../..', 'public')));
+    this.app.set('view engine', 'ejs');
+    this.app.use(logger('dev'));
 
-	this.app.use(function (err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: {}
-		});
-	});
-	this.CreateRoutes();
-	this.app.listen(this.port, this.ipaddress, function () {
-		//console.log('%s: Node server started on %s:%d ...',
-		//	Date(Date.now()), self.ipaddress, self.port);
-	});
+    /*app.use(function (req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });*/
+
+    if (this.app.get('env') === 'development') {
+        this.app.use(function (err, req, res, next) {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: err
+            });
+        });
+    }
+
+    this.app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+    this.CreateRoutes();
+    this.app.listen(this.port, this.ipaddress, function () {
+        //console.log('%s: Node server started on %s:%d ...',
+        //	Date(Date.now()), self.ipaddress, self.port);
+    });
 };
 
 Server.prototype.CreateRoutes = function () {
-	this.app.use('/', route);
+    this.app.use('/', route);
 };
 
 Server.prototype.setupVariables = function () {
-	this.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-	this.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    this.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+    this.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-	if (typeof this.ipaddress === "undefined") {
-		console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-		this.ipaddress = "127.0.0.1";
-	};
+    if (typeof this.ipaddress === "undefined") {
+        console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        this.ipaddress = "127.0.0.1";
+    };
 };
 
 Server.prototype.terminator = function (sig) {
-	if (typeof sig === "string") {
-		console.log('%s: Received %s - terminating sample app ...',
-			Date(Date.now()), sig);
-		process.exit(1);
-	}
-	console.log('%s: Node server stopped.', Date(Date.now()));
+    if (typeof sig === "string") {
+        console.log('%s: Received %s - terminating sample app ...',
+            Date(Date.now()), sig);
+        process.exit(1);
+    }
+    console.log('%s: Node server stopped.', Date(Date.now()));
 };
 
 
 
 Server.prototype.setupTerminationHandlers = function () {
-	process.on('exit', function () { this.terminator(); }.bind(this));
+    process.on('exit', function () { this.terminator(); }.bind(this));
 
-	['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-		'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-	].forEach(function (element, index, array) {
-		process.on(element, function () { this.terminator(element); }.bind(this));
-	}, this);
+    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+        'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+    ].forEach(function (element, index, array) {
+        process.on(element, function () { this.terminator(element); }.bind(this));
+    }, this);
 };
 
 module.exports = new Server();
